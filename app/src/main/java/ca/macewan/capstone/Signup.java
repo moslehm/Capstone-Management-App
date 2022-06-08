d1b0d1dbd8c1d1be1763ea9323b15bb79e121488
Neill Reyes <neillreyes@yahoo.ca>
2022-06-08 04:00:18 -0600

    Auth fix and validity checking

--- app/src/main/java/ca/macewan/capstone/Signup.java
+++ app/src/main/java/ca/macewan/capstone/Signup.java
@@ -1,6 +1,7 @@
 package ca.macewan.capstone;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,17 +17,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Signup extends AppCompatActivity {
-    private TextView error;
     private EditText emailField, passwordField, confirmPassword, nameField;
     private Button signup;
     private FirebaseAuth firebaseAuth;
     private FirebaseFirestore firebaseFirestore;
-    private String errorString;
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -37,21 +40,21 @@ public class Signup extends AppCompatActivity {
         emailField = findViewById(R.id.signUpEmail);
         passwordField = findViewById(R.id.signUpPassword);
         confirmPassword = findViewById(R.id.signUpConfirmPassword);
+        nameField = findViewById(R.id.signUpFullName);
         signup = findViewById(R.id.signUpButton);
-        error = findViewById(R.id.errorText);
 
-        firebaseAuth.signInAnonymously()
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = firebaseAuth.getCurrentUser();
-                        } else {
-                            Toast.makeText(getApplicationContext(), "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+//        firebaseAuth.signInAnonymously()
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            FirebaseUser user = firebaseAuth.getCurrentUser();
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+//                                    Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,45 +78,63 @@ public class Signup extends AppCompatActivity {
         String password = passwordField.getText().toString();
         String name = nameField.getText().toString();
         String email = emailField.getText().toString();
-        errorString = "";
 
         if (email.equals("")) {
-            errorString += "Email address cannot be empty.\n";
+            emailField.setError("This field can not be left blank");
         }
 
         if (name.equals ("")) {
-            errorString += "Full name cannot be empty.\n";
+            nameField.setError("This field can not be left blank");
         }
 
         if (password.equals("")) {
-            errorString += "Password cannot be empty.\n";
+            passwordField.setError("This field can not be left blank");
+        }
+
+        if (confirmPassword.getText().toString().equals("")) {
+            confirmPassword.setError("This field can not be left blank");
         }
 
         if (!password.equals(confirmPassword.getText().toString())) {
-            errorString += "Both passwords must match.\n";
+            confirmPassword.setError("Both passwords must match.");
         }
 
         if (!email.equals("")) {
             if (!email.endsWith("@mymacewan.ca") && !email.endsWith("@macewan.ca")) {
-                errorString += "Use a valid MacEwan email.\n";
+                emailField.setError("Must be a valid MacEwan email");
             }
         }
 
-        if (!errorString.isEmpty()) {
-            error.setText(errorString);
+        if (emailField.getError() != null || passwordField.getError() != null || nameField.getError() != null || confirmPassword.getError() != null) {
             return;
         }
 
-        firebaseFirestore = FirebaseFirestore.getInstance();
-        CollectionReference usersRef = firebaseFirestore.collection("Users");
-        User user = new User(email, name, email.endsWith("@macewan.ca") ? "professor" : "student");
-        usersRef.document(email).set(user);
-        firebaseAuth.createUserWithEmailAndPassword(email, password);
-        finish();
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    firebaseFirestore = FirebaseFirestore.getInstance();
+                    CollectionReference usersRef = firebaseFirestore.collection("Users");
+                    User user = new User(email, name, email.endsWith("@macewan.ca") ? "professor" : "student");
+                    usersRef.document(email).set(user);
+                    finish();
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (FirebaseAuthWeakPasswordException e) {
+                        passwordField.setError("Password should be at least 6 characters");
+                        Log.e(TAG, e.getMessage());
+                    } catch(FirebaseAuthUserCollisionException e) {
+                        emailField.setError("This email is already in use");
+                        Log.e(TAG, e.getMessage());
+                    } catch (FirebaseAuthInvalidCredentialsException e) {
+                        Log.e(TAG, e.getMessage());
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            }
+        });
     }
-
-    private void updateString(String string) {
-        errorString += string;
-    }
-
 }
